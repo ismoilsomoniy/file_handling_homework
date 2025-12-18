54c8131fd8a678b2008c5e6b51d6413c2e6d12ef
ismoilsomoniy <ismoilxushvaqtov05@gmail.com>
2025-12-18 17:45:29 +0500

    Add docstring to main function and clean up code
    Refactored main function to include docstring and removed redundant comments.

--- file05.py
+++ file05.py
@@ -1,6 +1,13 @@
 f=open("/home/ismoil/task/data/5.txt","r")
 str=f.read()
 def main(data:str):
+    """
+    The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
+    Args:
+        data: str
+    Returns:
+        list: return answer
+"""
     a=0
     dig=0
     for i in str:
@@ -10,10 +17,3 @@ def main(data:str):
             a+=1
     return [dig,a]
 print(main(str))
-"""
-    The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
-    Args:
-        data: str
-    Returns:
-        list: return answer
-"""
