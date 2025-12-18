56eed5b81261225b1a6a201a7f4308d800faca1e
ismoilsomoniy <ismoilxushvaqtov05@gmail.com>
2025-12-18 17:45:53 +0500

    Refactor docstring placement in main function
    Moved docstring for the main function inside the function and removed redundant comments.

--- file07.py
+++ file07.py
@@ -1,16 +1,16 @@
 f=open("/home/ismoil/task/data/7.txt","r")
 str=f.read()
 def main(data:str):
-    sum=0
-    for i in str:
-        if i.isdigit():
-            sum+=int(i)
-    return sum
-print(main(str))
-"""
+    """
     The data is from the file. Find a sum of numeric characters and return as list type.
     Args:
         data: str
     Returns:
         int: return answer
 """
+    sum=0
+    for i in str:
+        if i.isdigit():
+            sum+=int(i)
+    return sum
+print(main(str))
