0569b00bcf084eb6e0be23ea574a5b428a773d60
ismoilsomoniy <ismoilxushvaqtov05@gmail.com>
2025-12-18 17:45:14 +0500

    Refactor main function and improve docstring

--- file04.py
+++ file04.py
@@ -2,15 +2,15 @@ f=open("/home/ismoil/task/data/4.txt","r")
 str=f.read()
 l=[]
 def main(data:str):
-    for i in str:
-        if not i.isdigit():
-            l.append(i)
-    return l
-print(main(str))
-"""
+    """
     The data is from the file. Return the str(non-digital) characters as a list type.
     Args:
         data: str
     Returns:
         list: return answer
 """
+    for i in str:
+        if not i.isdigit():
+            l.append(i)
+    return l
+print(main(str))
