aa56bbd9be0362af55ed8015b11f6435894b9b98
ismoilsomoniy <ismoilxushvaqtov05@gmail.com>
2025-12-18 17:44:32 +0500

    Update file01.py

--- file01.py
+++ file01.py
@@ -2,12 +2,7 @@ f=open("/home/ismoil/task/data/1.txt","r")
 str=f.read()
 l=[]
 def main(data:str):
-    for i in str:
-        if i.isdigit():
-            l.append(int(i))
-    return l
-print(main(str))
-
+    
 """
     The data is from the file. Return data as a list type.
     Args:
@@ -15,3 +10,9 @@ print(main(str))
     Returns:
         list: return answer
 """
+    for i in str:
+        if i.isdigit():
+            l.append(int(i))
+    return l
+print(main(str))
+
