b191f4790a846eb2e53e5ab61d1178047a74b4ed
ismoilsomoniy <ismoilxushvaqtov05@gmail.com>
2025-12-18 17:44:45 +0500

    Update file02.py

--- file02.py
+++ file02.py
@@ -1,12 +1,12 @@
 f=open("/home/ismoil/task/data/2.txt","r")
 str=f.read()
 def main(data:str):
-    return len(str)
-print(main(str))
-"""
+    """
     The data is from the file. Return number of characters in the file.
     Args:
         data: str
     Returns:
         int: return answer
 """
+    return len(str)
+print(main(str))
