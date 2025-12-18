0fe6b98774a7b52a46221589ad956c2290b7a595
ismoilsomoniy <ismoilxushvaqtov05@gmail.com>
2025-12-18 17:44:59 +0500

    Update file03.py

--- file03.py
+++ file03.py
@@ -2,15 +2,15 @@ f=open("/home/ismoil/task/data/3.txt","r")
 str=f.read()
 l=[]
 def main(data:str):
-    for i in str:
-        if i.isdigit():
-            l.append(i)
-    return l
-print(main(str))
-"""
+    """
     The data is from the file. Return the digits as a list type.
     Args:
         data: str
     Returns:
         list: return answer
     """
+    for i in str:
+        if i.isdigit():
+            l.append(i)
+    return l
+print(main(str))
